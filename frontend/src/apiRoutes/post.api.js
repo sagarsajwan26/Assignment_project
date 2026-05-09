35aea4ae52721b22c3f9285b60a3a05141c74767
sagarsajwna26 <sagarsajwna26@gmail.com>
2026-05-09 18:03:05 +0530

    refactor: update frontend api routes to point to /api/stories

--- frontend/src/apiRoutes/post.api.js
+++ frontend/src/apiRoutes/post.api.js
@@ -1,6 +1,9 @@
 import api from '../api/axios';
 
-export const createPostApi = (data) => api.post('/posts', data);
-export const updatePostApi = (id, data) => api.put(`/posts/${id}`, data);
-export const deletePostApi = (id) => api.delete(`/posts/${id}`);
 export const getStoriesApi = (page = 1, limit = 10) => api.get(`/stories?page=${page}&limit=${limit}`);
+export const getStoryApi = (id) => api.get(`/stories/${id}`);
+export const toggleBookmarkApi = (id) => api.post(`/stories/${id}/bookmark`);
+export const getBookmarksApi = () => api.get('/stories/bookmarks');
+export const createPostApi = (data) => api.post('/stories', data);
+export const updatePostApi = (id, data) => api.put(`/stories/${id}`, data);
+export const deletePostApi = (id) => api.delete(`/stories/${id}`);
